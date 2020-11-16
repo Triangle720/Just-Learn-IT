8e2e043dd6512ef3aee57479a3b0028dc4b258bf
Przemysław Słoma <przemyslawsloma6@gmail.com>
2020-11-16 22:16:11 +0100

    + Authorization

---
+++ Security/AuthorizeFilter.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Linq;
+
+namespace JustLearnIT.Security
+{
+    public class RoleAuthFilter : TypeFilterAttribute
+    {
+        public RoleAuthFilter(string roleNames) : base(typeof(RoleRequirementFilter))
+        {
+            Arguments = new object[] { roleNames };
+        }
+    }
+
+    public class RoleRequirementFilter : IAuthorizationFilter
+    {
+        readonly string[] _roles;
+
+        public RoleRequirementFilter(string roles)
+        {
+            _roles = roles.Split(',');
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var hasClaim = _roles.Contains(context.HttpContext.Session.GetString("ROLE"));
+
+            if (!hasClaim)
+            {
+                context.Result = new ForbidResult();
+            }
+        }
+    }
+}
+
